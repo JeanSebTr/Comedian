e49914c28b71f893c8f703f5d964cc599a650976
Jean-Sébastien Tremblay <jeansebtr@gmail.com>
2015-12-03 20:30:30 -0500

    Add tests (1 failling)

--- Comedian.Tests/ActorCore/SyncCall.cs
+++ Comedian.Tests/ActorCore/SyncCall.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
 using System.Threading;
+using NSubstitute.ExceptionExtensions;
 
 namespace Comedian.Tests.ActorCore
 {
@@ -28,16 +29,53 @@ namespace Comedian.Tests.ActorCore
 			subject._actorCore.EnqueueAsync (VoidMethod);
 
 			var t = Task.Run (() => subject.GeneratedSyncMethod());
-			Thread.Sleep (10);
 
-			t.Status.Should ().Be (TaskStatus.Running);
+			t.Status.Should ().BeAnyOf (TaskStatus.WaitingToRun, TaskStatus.Running);
 			subject.Received (0).RealSyncMethod ();
 
 			subject._actorCore.ProcessNext ();
-			Thread.Sleep (10);
+
+			t.Wait (1000);
 
 			t.Status.Should ().Be (TaskStatus.RanToCompletion);
 			subject.Received (1).RealSyncMethod ();
+		}
+
+		[Test]
+		public void ShouldForwardExceptions()
+		{
+			var subject = Substitute.For<FakeActor> ();
+
+			subject.RealSyncMethod ().Throws<Exception> ();
+
+			new Action (() => subject.GeneratedSyncMethod ()).ShouldThrow<Exception> ();
+		}
+
+		[Test]
+		public void ShouldContinueProcessingOnException()
+		{
+			var subject = Substitute.For<FakeActor> ();
+
+			subject.RealSyncMethod ().Throws<Exception> ();
+
+			subject._actorCore.EnqueueAsync (VoidMethod);
+
+			var t1 = Task.Run (() => subject.GeneratedSyncMethod());
+			var t2 = Task.Run (() => subject.GeneratedSyncMethod());
+
+			t1.Status.Should ().BeAnyOf (TaskStatus.WaitingToRun, TaskStatus.Running);
+			t2.Status.Should ().BeAnyOf (TaskStatus.WaitingToRun, TaskStatus.Running);
+
+			subject.Received (0).RealSyncMethod ();
+
+			subject._actorCore.ProcessNext ();
+
+			var tasksWithoutExceptions = new []{t1.ContinueWith(t => {}), t2.ContinueWith(t => {})};
+
+			Task.WaitAll (tasksWithoutExceptions, 1000);
+
+			t1.Status.Should ().Be (TaskStatus.Faulted);
+			t2.Status.Should ().Be (TaskStatus.Faulted);
 		}
 
 		private void VoidMethod()
---
+++ Comedian.Tests/FluentAssertionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
+using System.Linq;
+
+namespace Comedian.Tests
+{
+	public static class FluentAssertionsExtensions
+	{
+		public static void BeAnyOf<TObject>(this ObjectAssertions assertions, params TObject[] expectations)
+		{
+			Execute.Assertion
+				.ForCondition(expectations.Any(exp => assertions.Subject.Equals(exp)))
+				.FailWith("Expected {context:object} to be any of {0}{reason}, but found {1}.", expectations, assertions.Subject);
+		}
+	}
+}
+
